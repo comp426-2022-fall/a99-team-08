09ff50fbde827aa6452ad8c450e8c0057b1ee9b2
Aditya Iyer <adityaiyer@unc.edu>
2022-12-08 01:01:17 -0500

    Added Login/Logout and Delete Account Features

--- index.js
+++ index.js
@@ -32,35 +32,86 @@ const dbPromise = open({
   driver: sqlite3.Database
 });
 
-app.get('/app/', (req, res) => {
-  res.render('home');
+app.get('/app/', async (req, res) => {
+  const { logged_in, username } = req.session;
+  //const user_info = await db.all();
+  if (logged_in) {
+    res.render('home', { username });
+  }
+  else {
+    res.redirect('/app/login/');
+  }
 });
 
 app.get('/app/register/', async (req, res) => {
-  const db = await dbPromise;
-  const teams = await db.all('SELECT * FROM Team;');
-  res.render('register', { teams });
+  const { logged_in, username } = req.session;
+  if (logged_in) {
+    res.redirect('/app/');
+  }
+  else {
+    const db = await dbPromise;
+    const teams = await db.all('SELECT * FROM Team;');
+    res.render('register', { teams });
+  }
 });
 
 app.post('/app/register/', async (req, res) => {
   const db = await dbPromise;
-  const { email, username, password, team } = req.body;
-  await db.run('INSERT INTO User (email, username, password, team) VALUES (?, ?, ?, ?)', email, username, password, team);
-  res.redirect('/app/');
+  const { email, username, password, passwordRepeat, team } = req.body;
+  if (password != passwordRepeat) {
+    const teams = await db.all('SELECT * FROM Team;');
+    res.render('register', { teams, error: "Passwords must match" });
+  }
+  else {
+    const users = await db.all('SELECT email, username FROM User WHERE email=? OR username=?;', email, username);
+    if (users.length > 0) {
+      const teams = await db.all('SELECT * FROM Team;');
+      res.render('register', { teams, error: "Email and/or username is already in use" });
+    }
+    else {
+      await db.run('INSERT INTO User (email, username, password, team) VALUES (?, ?, ?, ?);', email, username, password, team);
+      req.session.logged_in = true;
+      req.session.username = username;
+      res.redirect('/app/');
+    }
+  }
 });
 
-app.get('/app/login/', async (req, res) => {
-  const db = await dbPromise;
-  res.render('login');
+app.get('/app/login/', (req, res) => {
+  const { logged_in, username } = req.session;
+  if (logged_in) {
+    res.redirect('/app/')
+  }
+  else {
+    res.render('login');
+  }
 });
 
 app.post('/app/login/', async (req, res) => {
   const db = await dbPromise;
-  res.redirect('/app/');
+  const { username, password } = req.body;
+  const users = await db.all('SELECT email, username, team FROM User WHERE username=? AND password=?;', username, password);
+  if (users.length == 0) {
+    res.render("login", { error: "Invalid username or password" });
+  }
+  else {
+    req.session.logged_in = true;
+    req.session.username = username;
+    res.redirect('/app/');
+  }
 })
 
-app.post('/app/logout/', async (req, res) => {
+app.post('/app/logout/', (req, res) => {
+  req.session.logged_in = false;
+  req.session.username = undefined;
+  res.redirect('/app/login/');
+})
+
+app.post('/app/delete_account/', async (req, res) => {
   const db = await dbPromise;
+  await db.run('DELETE FROM User WHERE username=?;', req.session.username);
+  req.session.logged_in = false;
+  req.session.username = undefined;
   res.redirect('/app/login/');
 })
 
